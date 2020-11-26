739e770625d13b22adb3fe4db0c1d1640b921511
Ole Richter <ole.richter91@gmail.com>
2020-11-26 16:36:07 +0100

    add error component

---
+++ components/common/error.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+
+export default function Error() {
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => setShow(true), 0)
+  }, [])
+
+  return (
+    <div
+      className={`flex flex-col my-12 w-full ease-in-out duration-300 ${
+        show ? 'opacity-100' : 'opacity-0'
+      }`}
+    >
+      <div className="mx-auto">Oops. Something went wrong...</div>
+      <button
+        className="text-primary-600 hover:text-primary-500 mx-auto m-4 outline-none ease-in-out duration-200"
+        onClick={() => window.location.reload()}
+      >
+        <svg
+          className="w-10 h-10"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+          />
+        </svg>
+      </button>
+    </div>
+  )
+}
