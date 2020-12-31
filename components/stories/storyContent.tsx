4a139a0e4c5b908d1ff16fec7836fa0a08b9ddf9
Ole Richter <ole.richter91@gmail.com>
2020-12-31 10:03:50 +0100

    increase text size

--- components/stories/storyContent.tsx
+++ components/stories/storyContent.tsx
@@ -26,7 +26,7 @@ export default function StoryContent({
   commentsCount,
 }: StoryContentProps) {
   return (
-    <div className="flex flex-col justify-between ml-3 mr-4 mt-4 h-full text-sm leading-normal sm:mr-3 sm:mt-0 sm:mx-0 sm:pl-4 sm:pr-0 sm:pt-0 sm:w-full sm:text-tiny">
+    <div className="flex flex-col justify-between ml-3 mr-4 mt-4 h-full text-tiny leading-normal sm:mr-3 sm:mt-0 sm:mx-0 sm:pl-4 sm:pr-0 sm:pt-0 sm:w-full">
       <div className="text-neutral hover:text-primary text-base font-bold leading-tight sm:text-lg sm:font-semibold">
         <Truncate lines={titleLines} ellipsis={'...'} onTruncate={onTruncateTitle}>
           {title}
