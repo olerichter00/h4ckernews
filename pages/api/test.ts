1b2d1cf78fcf998ab82a6eb1f1f2683f86ac38ae
Ole Richter <ole.richter91@gmail.com>
2021-01-04 19:27:01 +0100

    Delete test.ts

--- pages/api/test.ts
+++
@@ -1,6 +0,0 @@
-import { NowRequest, NowResponse } from '@vercel/node'
-
-export default async (req: NowRequest, res: NowResponse) => {
-  res.setHeader('Cache-Control', 'public, s-max-age=0, stale-while-revalidate')
-  res.send(new Date())
-}
