993f0d15c1021d2a8c3d50809825ea26bbce3400
Ole Richter <ole.richter91@gmail.com>
2021-01-12 17:24:16 +0100

    introduce new story list transition logic

--- components/stories/storyList.tsx
+++ components/stories/storyList.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Loadable } from 'recoil'
+import React, { useEffect, useMemo, useState } from 'react'
+import { Loadable, useRecoilValue } from 'recoil'
 import FadeTransition from '../common/fadeTransition'
 
 import Spinner from '../common/spinner'
-import Error from '../common/errorMessage'
+import ErrorMessage from '../common/errorMessage'
 import NoStories from '../common/noStories'
 import Story from './story'
-import { TStory, Type } from '../../lib/store/recoil'
+import { filterState, TStory, Type } from '../../lib/store/recoil'
 
 export type StoryListProps = {
   stories: Loadable<TStory[]>
@@ -14,16 +14,48 @@ export type StoryListProps = {
   count: number
 }
 export default function StoryList({ stories, type, count }: StoryListProps) {
-  if (stories.state === 'loading') return <Spinner />
-  if (stories.state === 'hasError') return <Error />
+  const [refresh, setRefresh] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const [showSpinner, setShowSpinner] = useState(false)
+  const [showSpinnerTimeout, setShowSpinnerTimeout] = useState<any>(undefined)
+  const filter = useRecoilValue(filterState)
 
-  if (stories.contents.length === 0) return <NoStories />
+  useEffect(() => {
+    setLoading(true)
+    if (stories.state !== 'loading') setTimeout(() => setLoading(false), 100)
+  }, [stories.state])
+
+  useEffect(() => {
+    clearTimeout(showSpinnerTimeout)
+    setShowSpinner(false)
+
+    if (stories.state === 'loading') {
+      setShowSpinnerTimeout(setTimeout(() => setShowSpinner(true), 500))
+    }
+  }, [stories.state])
+
+  useEffect(() => {
+    if (stories.state !== 'loading') {
+      setRefresh(refresh + 1)
+    }
+  }, [stories.state, filter])
+
+  const storiesContent = useMemo(() => (stories.state === 'hasValue' ? stories.contents : []), [
+    refresh,
+  ])
+
+  if (showSpinner) return <Spinner />
+
+  if (stories.state === 'hasValue' && stories.contents.length === 0) return <ErrorMessage />
+  if (stories.state === 'hasError') return <ErrorMessage />
 
   return (
-    <div className="sm:mx-3">
-      {stories.contents.slice(0, count).map(story => {
+    <div
+      className={`duration-500 transition-opacity sm:mx-3 ${loading ? 'opacity-0' : 'opacity-100'}`}
+    >
+      {storiesContent.slice(0, count).map(story => {
         return (
-          <FadeTransition key={`${type}-${story.id}`}>
+          <FadeTransition key={`${story.id}-${refresh}`}>
             <Story story={story} />
           </FadeTransition>
         )
