dc2abcc59548c33e451d372b5977c55516778585
Ole Richter <ole.richter91@gmail.com>
2021-01-21 13:36:07 +0100

    Delete refreshStories.test.ts

--- scripts/refreshStories.test.ts
+++
@@ -1,7 +0,0 @@
-import config from '../lib/config'
-import updateStories from '../lib/services/updateStories'
-import { StoryType } from '../lib/types'
-
-test('updates stories', async () => {
-  await Promise.allSettled(config.storyTypes.map((type: StoryType) => updateStories(type)))
-})
