91f162d3a031ad826adb337d1dab2c10e3c9250d
Ole Richter <ole.richter91@gmail.com>
2021-01-25 10:25:12 +0100

    fix story sorting

--- lib/repositories/storyRepository.ts
+++ lib/repositories/storyRepository.ts
@@ -4,16 +4,24 @@ import { Story as IStory } from 'lib/types'
 
 @injectable()
 class StoryRepository {
-  findStory = async (id: string): Promise<IStory[] | null> => {
+  public findStory = async (id: string): Promise<IStory[] | null> => {
     return Story.findOne({ id }).exec()
   }
 
-  findStories = async (ids: string[]): Promise<IStory[]> => {
-    return Story.find({ id: { $in: ids } })
+  public findStories = async (ids: string[]): Promise<IStory[]> => {
+    const stories = await Story.find({ id: { $in: ids } })
+
+    return this.sortStories(stories, ids)
   }
 
-  createStory = async (story: IStory): Promise<IStory> => {
+  public createStory = async (story: IStory): Promise<IStory> => {
     return Story.create(story)
+  }
+
+  private sortStories = (stories: IStory[], ids: string[]): IStory[] => {
+    return stories.sort((a, b) => {
+      return ids.findIndex(id => a.id === String(id)) - ids.findIndex(id => b.id === String(id))
+    })
   }
 }
 
