a26ae0416808d7b16bfa6f51e0db95a483f188ee
Ole Richter <ole.richter91@gmail.com>
2021-02-03 15:34:46 +0100

    Create noStoriesMessage.tsx

---
+++ components/elements/noStoriesMessage.tsx
@@ -0,0 +1,39 @@
+import { filterState } from 'lib/store/recoil'
+import React, { useState, useEffect } from 'react'
+import { useRecoilState } from 'recoil'
+
+const NoStoriesMessage: React.FC = () => {
+  const [show, setShow] = useState(false)
+  const [, setFilter] = useRecoilState(filterState)
+
+  useEffect(() => {
+    setTimeout(() => setShow(true), 0)
+  }, [])
+
+  return (
+    <div className={`flex flex-col my-12 w-full ${show ? 'opacity-100' : 'opacity-0'}`}>
+      <div className="mx-auto">{"Couldn't find any stories..."}</div>
+      <button
+        className="dark:hover:text-gray-300 hover:text-700 m-4 mx-auto text-primary outline-none"
+        onClick={() => setFilter(false)}
+      >
+        <svg
+          className="w-10 h-10"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+          />
+        </svg>
+      </button>
+    </div>
+  )
+}
+
+export default NoStoriesMessage
